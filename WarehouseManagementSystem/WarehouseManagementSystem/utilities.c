1511396735b09ca472445cb79388c535e0f7d688
Keshav Kumar <marrkann08@gmail.com>
2026-07-30 22:58:06 -0400

    Implement Utilities numeric conversion functions

--- WarehouseManagementSystem/WarehouseManagementSystem/utilities.c
+++ WarehouseManagementSystem/WarehouseManagementSystem/utilities.c
@@ -2,6 +2,9 @@
 
 #include <ctype.h>
 #include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
 
 int util_validate_string(char* text, size_t max_length)
 {
@@ -70,6 +73,111 @@ int util_check_null(const void* pointer)
     {
         return -1;
     }
+
+    return 0;
+}
+
+int util_string_to_int(
+    const char* text,
+    int minimum,
+    int maximum,
+    int* out_value
+)
+{
+    char* conversion_end;
+    long converted_value;
+
+    if (text == NULL || out_value == NULL || minimum > maximum)
+    {
+        return -1;
+    }
+
+    errno = 0;
+    converted_value = strtol(text, &conversion_end, 10);
+
+    if (conversion_end == text || errno == ERANGE)
+    {
+        return -1;
+    }
+
+    /* Allow trailing whitespace, but reject other trailing characters. */
+    while (*conversion_end != '\0' &&
+        isspace((unsigned char)*conversion_end))
+    {
+        ++conversion_end;
+    }
+
+    if (*conversion_end != '\0')
+    {
+        return -1;
+    }
+
+    if (converted_value < minimum || converted_value > maximum)
+    {
+        return -1;
+    }
+
+    *out_value = (int)converted_value;
+
+    return 0;
+}
+
+int util_string_to_uint(
+    const char* text,
+    unsigned int minimum,
+    unsigned int maximum,
+    unsigned int* out_value
+)
+{
+    const char* first_character;
+    char* conversion_end;
+    unsigned long converted_value;
+
+    if (text == NULL || out_value == NULL || minimum > maximum)
+    {
+        return -1;
+    }
+
+    first_character = text;
+
+    while (isspace((unsigned char)*first_character))
+    {
+        ++first_character;
+    }
+
+    /* strtoul can accept a minus sign, so reject it explicitly. */
+    if (*first_character == '-')
+    {
+        return -1;
+    }
+
+    errno = 0;
+    converted_value = strtoul(text, &conversion_end, 10);
+
+    if (conversion_end == text ||
+        errno == ERANGE ||
+        converted_value > UINT_MAX)
+    {
+        return -1;
+    }
+
+    while (*conversion_end != '\0' &&
+        isspace((unsigned char)*conversion_end))
+    {
+        ++conversion_end;
+    }
+
+    if (*conversion_end != '\0')
+    {
+        return -1;
+    }
+
+    if (converted_value < minimum || converted_value > maximum)
+    {
+        return -1;
+    }
+
+    *out_value = (unsigned int)converted_value;
 
     return 0;
 }
