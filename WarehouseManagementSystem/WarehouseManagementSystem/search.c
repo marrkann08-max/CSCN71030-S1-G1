fcbeb863f6a5699b67fc6df98ef787690fe5fc86
ihundal <ihundal8478@conestogac.on.ca>
2026-08-05 15:03:38 -0400

    Implemmented failing unit test cases

--- WarehouseManagementSystem/WarehouseManagementSystem/search.c
+++ WarehouseManagementSystem/WarehouseManagementSystem/search.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"search.h"
+static SearchHistoryEntry history[SEARCH_HISTORY_CAPACITY];
+static int historyCount = 0;
 Product* search_by_id(Product* head, unsigned int productID) {
 	return NULL;
 }
@@ -17,5 +19,14 @@ int search_all_products(Product* head, SearchResult* result) {
 	return 0;
 }
 void search_free_results(SearchResult* result) {
+
+}
+int search_record_history(const char* criteria, int resultCount) {
+	return 0;
+}
+int search_get_history(SearchHistoryEntry history[]) {
+	return 0;
+}
+void search_clear_history(void) {
 
 }
--- WarehouseManagementSystem/WarehouseManagementSystem/search.h
+++ WarehouseManagementSystem/WarehouseManagementSystem/search.h
@@ -3,6 +3,13 @@
 #define SEARCH_H
 #define PRODUCT_NAME_LENGTH 64
 #define PRODUCT_LOCATION_LENGTH 32
+#define SEARCH_HISTORY_CAPACITY 10
+#define SEARCH_TEXT_LENGTH 64
+typedef struct
+{
+    char criteria[SEARCH_TEXT_LENGTH];
+    int resultCount;
+} SearchHistoryEntry;
 typedef struct Product
 {
     unsigned int id;
@@ -21,4 +28,7 @@ int search_below_threshold(Product* head, int threshold, SearchResult* result);
 int search_by_quantity_range(Product* head, int minimum, int maximum, SearchResult* result);
 int search_all_products(Product* head, SearchResult* result);
 void search_free_results(SearchResult* result);
+int search_record_history(const char* criteria,int resultCount);
+int search_get_history(SearchHistoryEntry history[]);
+void search_clear_history(void);
 #endif
