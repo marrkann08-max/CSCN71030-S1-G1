b2a575a316bae80e3aec59ff9cff362a218bf7db
ivanimmanuel-dev <ivanimmanuel1234@gmail.com>
2026-08-06 02:06:27 -0400

    test(config): added native Config test project

---
+++ WarehouseManagementSystem/Configtests/Configtests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Configtests
+{
+	TEST_CLASS(Configtests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
